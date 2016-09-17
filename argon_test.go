bab596635427255c590a697bddb8c777441e235c
bunsim <bunsim@protonmail.com>
2016-09-17 12:34:06 -0400

    fix failing test

--- argon_test.go
+++ argon_test.go
@@ -6,8 +6,6 @@ func BenchmarkArgon(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		pwd := make([]byte, 8)
 		RandBytes(pwd)
-		salt := make([]byte, PasswordSaltLen)
-		RandBytes(salt)
-		PasswordHash(pwd, salt, 5, 64*1024*1024)
+		PasswordHash(pwd, 5, 64*1024*1024)
 	}
 }
